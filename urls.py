0c596fb2ddb772b25c598219ee50f479d5f29984
Alexandr D. Kanevskiy <kad@kad.name>
2009-04-21 17:40:22 +0300

    enable admin interface

--- urls.py
+++ urls.py
@@ -7,17 +7,21 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+#from bendersproperty.fb.models import User
+from bendersproperty.bp.models import User
+admin.site.register(User)
 
 urlpatterns = patterns('',
     # Example:
-    (r'^benders-property/', include('bendersproperty.bp.urls'))
+    (r'^benders-property/', include('bendersproperty.bp.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
