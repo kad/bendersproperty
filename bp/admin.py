e46ac28a9e1b8d862d87b3509349aa1ff83d87c6
Alexandr D. Kanevskiy <kad@kad.name>
2009-05-22 00:18:44 +0300

    beatify admin

--- bp/admin.py
+++ bp/admin.py
@@ -6,6 +6,16 @@
 from django.contrib import admin
 
 from models import FBUser, Trade, LandRegion, PropertyType, PropertyKind
-for cls in (FBUser, Trade, LandRegion, PropertyType, PropertyKind):
+for cls in (Trade, LandRegion, PropertyType):
     admin.site.register(cls)
 
+class FBUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cash', 'value', 'rent', 'hour_rate', 'max_cash', 'max_value')
+    ordering = ('-value', '-cash', '-rent', 'id', )
+
+class PropertyKindAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size', 'value', 'cost', 'rent', 'require_builder', 'require_plumber', 'require_electrician', 'require_plasterer', 'require_decorator')
+    ordering = ('-size', '-value', '-rent', '-cost', )
+
+admin.site.register(FBUser, FBUserAdmin)
+admin.site.register(PropertyKind, PropertyKindAdmin)
